76ded250dde8e3f7081d7a8b7d34de2754c46008
GuoSong <1424956666@qq.com>
2024-02-25 10:28:33 +0800

    update linear_type.py

--- lib/linear_type.py
+++ lib/linear_type.py
@@ -5,39 +5,39 @@ import torch.nn.functional as F
 from .options import args
 N = args.N
 M = args.M
-class GetMask(autograd.Function):
-    @staticmethod
-    def forward(ctx, mask, prune_rate):
-        out = mask.clone()
-        _, idx = mask.abs().flatten().sort()
-        j = int(prune_rate * mask.numel())
+# class GetMask(autograd.Function):
+#     @staticmethod
+#     def forward(ctx, mask, prune_rate):
+#         out = mask.clone()
+#         _, idx = mask.abs().flatten().sort()
+#         j = int(prune_rate * mask.numel())
 
-        # flat_out and out access the same memory.
-        flat_out = out.flatten()
-        flat_out[idx[:j]] = 0
-        flat_out[idx[j:]] = 1
-        return out
+#         # flat_out and out access the same memory.
+#         flat_out = out.flatten()
+#         flat_out[idx[:j]] = 0
+#         flat_out[idx[j:]] = 1
+#         return out
 
-    @staticmethod
-    def backward(ctx, g):
-        # send the gradient g straight-through on the backward pass.
-        return g, None
-class GetMaskNM(autograd.Function):
-    @staticmethod
-    def forward(ctx, mask):
-        out = mask.clone()
-        length = mask.numel()
-        group = int(length / M)
-        out_regroup = out.view(group, M)
-        indices = torch.argsort(out_regroup.abs(), dim=1,descending=True)[:, :N]
-        out_regroup = out_regroup.zero_().scatter_(1, indices.cuda(), 1)
-        return out
+#     @staticmethod
+#     def backward(ctx, g):
+#         # send the gradient g straight-through on the backward pass.
+#         return g, None
+# class GetMaskNM(autograd.Function):
+#     @staticmethod
+#     def forward(ctx, mask):
+#         out = mask.clone()
+#         length = mask.numel()
+#         group = int(length / M)
+#         out_regroup = out.view(group, M)
+#         indices = torch.argsort(out_regroup.abs(), dim=1,descending=True)[:, :N]
+#         out_regroup = out_regroup.zero_().scatter_(1, indices.cuda(), 1)
+#         return out
 
-    @staticmethod
-    def backward(ctx, g):
-        # send the gradient g straight-through on the backward pass.
-        return g, None
-# Not learning weights, finding lottery jackpots
+#     @staticmethod
+#     def backward(ctx, g):
+#         # send the gradient g straight-through on the backward pass.
+#         return g, None
+
 class LinearMasked(nn.Linear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
